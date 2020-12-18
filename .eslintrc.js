85967cc832935f395e91c7cfb97ad99c1438be90
Matthew Styers <matthew@styers.co>
2020-12-18 17:29:03 -0600

    fixes eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,56 @@
 module.exports = {
-    'env': {
-        'es6': true,
-        'node': true,
-        'mocha': true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "eslint:recommended",
-        // 'airbnb'
-    ],
-    'parserOptions': {
-        'ecmaVersion': 2015,
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': ['error', 2],
-        'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'never'],
-        'import/no-unresolved': 'off',
-        'no-underscore-dangle': 'off',
-        'guard-for-in': 'off',
-        'no-restricted-syntax': 'off',
-        'no-await-in-loop': 'off',
-        'object-curly-newline': 'off'
-    },
-    overrides: [
-        {
-          files: ['*-test.js', '*.spec.js'],
-          rules: {
-            'no-unused-expressions': 'off',
-            'func-names': 'off',
-            'prefer-arrow-callback': 'off',
-            "react/jsx-filename-extension": 'off'
-          }
-        }
-    ],
-    globals: {
-        'expect': true,
+  'env': {
+    es2020: true,
+    node: true,
+    mocha: true
+  },
+  'extends': ['plugin:vue/essential', 'eslint:recommended'],
+  'parserOptions': {
+    // ecmaVersion: 2015,
+    // sourceType: 'module',
+    ecmaFeatures: {
+      arrowFunctions: true,
+      blockBindings: true,
+      classes: true,
+      defaultParams: true,
+      destructuring: true,
+      forOf: true,
+      generators: false,
+      modules: true,
+      objectLiteralComputedProperties: true,
+      objectLiteralDuplicateProperties: false,
+      objectLiteralShorthandMethods: true,
+      objectLiteralShorthandProperties: true,
+      spread: true,
+      superInFunctions: true,
+      templateStrings: true,
+      jsx: true
     }
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'import/no-unresolved': 'off',
+    'no-underscore-dangle': 'off',
+    'guard-for-in': 'off',
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
+    'object-curly-newline': 'off'
+  },
+  'overrides': [
+    {
+      files: ['*-test.js', '*.spec.js'],
+      rules: {
+        'no-unused-expressions': 'off',
+        'func-names': 'off',
+        'prefer-arrow-callback': 'off',
+        'react/jsx-filename-extension': 'off'
+      }
+    }
+  ],
+  'globals': {
+    expect: true,
+  }
 }
